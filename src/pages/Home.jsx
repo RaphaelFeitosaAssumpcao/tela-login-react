ba23c2221bf3d854e3b98455755b44bf2edc1cec
Raphael Pessoal <raphaelassumpcao00@gmail.com>
2026-03-20 01:47:06 -0300

    criando-front-cadastro
    Tela de home criacao de colab

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,28 +8,40 @@ function Home() {
   const [search, setSearch] = useState("");
   const [statusFilter, setStatusFilter] = useState("Todos");
 
+  const [formData, setFormData] = useState({
+    nome: "",
+    email: "",
+    cargo: "",
+    sala: "",
+    gestor: "",
+    diretor: "",
+    status: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const totalColaboradores = colaboradoresMock.length;
 
   const totalAtivos = colaboradoresMock.filter(
-    (colaborador) => colaborador.status === "Ativo",
+    (c) => c.status === "Ativo",
   ).length;
 
   const totalInativos = colaboradoresMock.filter(
-    (colaborador) => colaborador.status === "Inativo",
+    (c) => c.status === "Inativo",
   ).length;
 
-  const totalSetores = new Set(
-    colaboradoresMock.map((colaborador) => colaborador.sala),
-  ).size;
+  const totalSetores = new Set(colaboradoresMock.map((c) => c.sala)).size;
 
   const colaboradoresFiltrados = useMemo(() => {
-    return colaboradoresMock.filter((colaborador) => {
-      const matchNome = colaborador.nome
-        .toLowerCase()
-        .includes(search.toLowerCase());
+    return colaboradoresMock.filter((c) => {
+      const matchNome = c.nome.toLowerCase().includes(search.toLowerCase());
 
-      const matchStatus =
-        statusFilter === "Todos" || colaborador.status === statusFilter;
+      const matchStatus = statusFilter === "Todos" || c.status === statusFilter;
 
       return matchNome && matchStatus;
     });
@@ -66,6 +78,94 @@ function Home() {
           description="Áreas diferentes encontradas no mock"
         />
       </section>
+      <div className="register">
+        <div className="header">
+          <p>Cadastro de Colaboradores</p>
+          <button className="button" type="button">
+            Adicionar
+          </button>
+        </div>
+        <div className="register-colab">
+          <form className="form-grid">
+            <div className="form-group">
+              <label>Nome</label>
+              <input
+                name="nome"
+                value={formData.nome}
+                onChange={handleChange}
+                type="text"
+                placeholder="Digite o nome"
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Email</label>
+              <input
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                type="text"
+                placeholder="Digite o email"
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Cargo</label>
+              <input
+                name="cargo"
+                value={formData.cargo}
+                onChange={handleChange}
+                type="text"
+                placeholder="Digite o cargo"
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Sala</label>
+              <input
+                name="sala"
+                value={formData.sala}
+                onChange={handleChange}
+                type="text"
+                placeholder="Digite a sala"
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Gestor</label>
+              <input
+                name="gestor"
+                value={formData.gestor}
+                onChange={handleChange}
+                type="text"
+                placeholder="Digite o gestor"
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Diretor</label>
+              <input
+                name="diretor"
+                value={formData.diretor}
+                onChange={handleChange}
+                type="text"
+                placeholder="Digite o diretor"
+              />
+            </div>
+
+            <div className="form-group full-width">
+              <label>Status</label>
+              <input
+                name="status"
+                value={formData.status}
+                onChange={handleChange}
+                type="text"
+                placeholder="Ativo / Inativo"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
     </section>
   );
 }
